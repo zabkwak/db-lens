7b4e01e9a3c61510fcda51eaa1077d337f75d0d7
zabkwak <lukas.macuda@gmail.com>
2025-08-21 17:46:40 +0200

    fixed ui table cells with without data

--- webview-ui/src/vscode-api.ts
+++ webview-ui/src/vscode-api.ts
@@ -28,179 +28,183 @@ export const vscode =
 					}
 					if (message.command === 'query') {
 						window.postMessage({
-							command: 'result',
+							command: 'query.result',
 							payload: {
-								data: [
-									{
-										id: 'account-1',
-										name: 'Account 1',
-										'column-1': 'Some longer string to wrap the fucking lines',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-									{
-										id: 'account-1',
-										name: 'Account something something 1',
-										'column-1': 'Something',
-									},
-								],
-								columns: [
-									{
-										name: 'id',
-									},
-									{
-										name: 'name',
-									},
-									{
-										name: 'column-1',
-									},
-									{
-										name: 'column-2',
-									},
-									{
-										name: 'column-3',
-									},
-									{
-										name: 'column-4',
-									},
-									{
-										name: 'column-5',
-									},
-									{
-										name: 'column-6',
-									},
-									{
-										name: 'column-7',
-									},
-									{
-										name: 'column-8',
-									},
-									{
-										name: 'column-9',
-									},
-									{
-										name: 'column-10',
-									},
-									{
-										name: 'column-11',
-									},
-									{
-										name: 'column-12',
-									},
-									{
-										name: 'column-13',
-									},
-									{
-										name: 'column-14',
-									},
-									{
-										name: 'column-15',
-									},
-									{
-										name: 'column-16',
-									},
-									{
-										name: 'column-17',
-									},
-									{
-										name: 'column-18',
-									},
-									{
-										name: 'column-19',
-									},
-									{
-										name: 'column-20',
-									},
-								],
+								success: true,
+								data: {
+									data: [
+										{
+											id: 'account-1',
+											name: 'Account 1',
+											'column-1': 'Some longer string to wrap the fucking lines',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+										{
+											id: 'account-1',
+											name: 'Account something something 1',
+											'column-1': 'Something',
+										},
+									],
+									columns: [
+										{
+											name: 'id',
+										},
+										{
+											name: 'name',
+										},
+										{
+											name: 'column-1',
+										},
+										{
+											name: 'column-2',
+										},
+										{
+											name: 'column-3',
+										},
+										{
+											name: 'column-4',
+										},
+										{
+											name: 'column-5',
+										},
+										{
+											name: 'column-6',
+										},
+										{
+											name: 'column-7',
+										},
+										{
+											name: 'column-8',
+										},
+										{
+											name: 'column-9',
+										},
+										{
+											name: 'column-10',
+										},
+										{
+											name: 'column-11',
+										},
+										{
+											name: 'column-12',
+										},
+										{
+											name: 'column-13',
+										},
+										{
+											name: 'column-14',
+										},
+										{
+											name: 'column-15',
+										},
+										{
+											name: 'column-16',
+										},
+										{
+											name: 'column-17',
+										},
+										{
+											name: 'column-18',
+										},
+										{
+											name: 'column-19',
+										},
+										{
+											name: 'column-20',
+										},
+									],
+								},
 							},
+							requestId: 'test',
 						});
 						return;
 					}
