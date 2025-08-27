982a44eeffe3546b0169ab607c0e06929df0a4cf
zabkwak <lukas.macuda@gmail.com>
2025-08-27 09:02:15 +0200

    test coverage for extension tests #65 (#68)

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,13 +1,29 @@
 import { defineConfig } from '@vscode/test-cli';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export default defineConfig({
-	// files: ["out/__tests__/**/*.test.js", "out/**/__tests__/**/*.test.js"],
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/**
+ * @type {import('@vscode/test-cli').IConfigurationWithGlobalOptions}
+ */
+const test = {
 	files: ['out/src/extension/__tests__/**/*.test.js', 'out/src/extension/**/__tests__/**/*.test.js'],
 	srcDir: 'src',
-	// coverage: ['src/extension/**/*.ts'],
-	// debug: true,
+	coverage: {
+		// include: ['src/extension/**/*.ts'],
+		// exclude: [`${__dirname}/src/**/__tests__/**/*.test.ts`, '../../shared/**/*.ts'],
+		reporter: ['json', 'html'],
+		// includeAll: true,
+	},
 	mocha: {
 		require: 'source-map-support/register.js',
 		ui: 'bdd',
 	},
+};
+
+export default defineConfig({
+	coverage: test.coverage,
+	tests: [test],
 });
