3b33f912bfa683b46c5918afbab20369782fa278
zabkwak <lukas.macuda@gmail.com>
2025-09-07 13:54:57 +0200

    root connection group #49

--- src/connection/connection-group.ts
+++ src/connection/connection-group.ts
@@ -1,22 +1,44 @@
-import Connection, { IConnectionGroup } from './connection';
+import Logger from '../logger';
+import Connection, { IConnection, IConnectionGroup } from './connection';
 
 export default class ConnectionGroup {
+	public static createRoot(children: (IConnection<any, any> | IConnectionGroup)[]): ConnectionGroup {
+		const root = new ConnectionGroup({
+			name: '__ROOT__',
+			connections: children,
+		});
+		root._isRoot = true;
+		return root;
+	}
+
 	private _name: string;
 	private _children: (Connection<any, any> | ConnectionGroup)[];
+	private _isRoot: boolean = false;
 
 	constructor(config: IConnectionGroup) {
 		this._name = config.name;
-		this._children = config.connections.map((connection) => {
-			if ('connections' in connection) {
-				return new ConnectionGroup(connection);
-			}
-			return new Connection(connection);
-		});
+		this._children = config.connections
+			.map((connection) => {
+				if ('connections' in connection) {
+					return new ConnectionGroup(connection);
+				}
+				try {
+					return new Connection(connection);
+				} catch (error: any) {
+					Logger.error('connection', `Failed to create connection ${config.name}: ${error.message}`);
+					return null;
+				}
+			})
+			.filter(Boolean) as (Connection<any, any> | ConnectionGroup)[];
 	}
 
 	public addChild(child: Connection<any, any> | ConnectionGroup): void {
 		if (this._children.find((c) => c.getName() === child.getName())) {
-			throw new Error(`Child with name ${child.getName()} already exists in group ${this._name}`);
+			throw new Error(
+				this._isRoot
+					? `Connection with name ${child.getName()} already exists`
+					: `Child with name ${child.getName()} already exists in group ${this._name}`,
+			);
 		}
 		this._children.push(child);
 	}
@@ -24,7 +46,11 @@ export default class ConnectionGroup {
 	public updateChild(child: Connection<any, any> | ConnectionGroup): void {
 		const index = this._children.findIndex((c) => c.getName() === child.getName());
 		if (index === -1) {
-			throw new Error(`Child with name ${child.getName()} not found in group ${this._name}`);
+			throw new Error(
+				this._isRoot
+					? `Connection with name ${child.getName()} does not exist`
+					: `Child with name ${child.getName()} not found in group ${this._name}`,
+			);
 		}
 		this._children[index] = child;
 	}
