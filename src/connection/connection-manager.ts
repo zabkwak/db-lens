76cb3d2c6079da7e15beffd72a7bbbde1ffcd3e6
zabkwak <lukas.macuda@gmail.com>
2025-09-07 13:58:21 +0200

    root connection group #49 (#93)
    * root connection group #49
    
    * fixed error message

--- src/connection/connection-manager.ts
+++ src/connection/connection-manager.ts
@@ -8,31 +8,18 @@ import Logger from '../logger';
 import ConnectionGroup from './connection-group';
 
 export default class ConnectionManager {
-	// TODO move this to root connection group?
-	private static _connections: (Connection<any, any> | ConnectionGroup)[] | null = null;
+	private static _root: ConnectionGroup | null = null;
 
 	public static async load(): Promise<void> {
 		const configPath = this.getConnectionsFilePath();
 		try {
 			const data = await fs.readFile(configPath, 'utf-8');
 			const connectionsData: (IConnection<any, any> | IConnectionGroup)[] = JSON.parse(data);
-			this._connections = connectionsData
-				.map((config) => {
-					if ('connections' in config) {
-						return new ConnectionGroup(config);
-					}
-					try {
-						return new Connection(config);
-					} catch (error: any) {
-						Logger.error('connection', `Failed to create connection ${config.name}: ${error.message}`);
-						return null;
-					}
-				})
-				.filter(Boolean) as Connection<any, any>[];
+			this._root = ConnectionGroup.createRoot(connectionsData);
 		} catch (error: any) {
 			if (error.code === 'ENOENT') {
 				Logger.info('connection', `No connections file found at ${configPath}.`);
-				this._connections = [];
+				this._root = ConnectionGroup.createRoot([]);
 			} else {
 				Logger.error('connection', `Failed to load connections: ${error.message}`);
 				vscode.window.showErrorMessage(`Failed to load connections: ${error.message}`);
@@ -44,18 +31,14 @@ export default class ConnectionManager {
 	public static addConnection(connection: Connection<any, any>): Promise<void>;
 	public static addConnection(connection: Connection<any, any>, groupsPath: string[]): Promise<void>;
 	public static async addConnection(connection: Connection<any, any>, groupsPath?: string[]): Promise<void> {
-		if (!this._connections) {
+		if (!this._root) {
 			await this.load();
 		}
-		assert(this._connections, 'Connections should be loaded before adding a new one');
+		assert(this._root, 'Connections should be loaded before adding a new one');
 		if (!groupsPath?.length) {
-			const existingConnection = this._connections.find((c) => c.getName() === connection.getName());
-			if (existingConnection) {
-				throw new Error(`Connection with name ${connection.getName()} already exists`);
-			}
-			this._connections.push(connection);
+			this._root.addChild(connection);
 		} else {
-			const group = this._findGroupByPath(groupsPath, this._connections);
+			const group = this._findGroupByPath(groupsPath, this._root.getChildren());
 			if (!group) {
 				throw new Error(`Group path ${groupsPath.join(' -> ')} not found`);
 			}
@@ -67,18 +50,14 @@ export default class ConnectionManager {
 	public static updateConnection(connection: Connection<any, any>): Promise<void>;
 	public static updateConnection(connection: Connection<any, any>, groupsPath: string[]): Promise<void>;
 	public static async updateConnection(connection: Connection<any, any>, groupsPath?: string[]): Promise<void> {
-		if (!this._connections) {
+		if (!this._root) {
 			await this.load();
 		}
-		assert(this._connections, 'Connections should be loaded before updating one');
+		assert(this._root, 'Connections should be loaded before updating one');
 		if (!groupsPath?.length) {
-			const index = this._connections.findIndex((c) => c.getName() === connection.getName());
-			if (index === -1) {
-				throw new Error(`Connection with name ${connection.getName()} does not exist`);
-			}
-			this._connections[index] = connection;
+			this._root.updateChild(connection);
 		} else {
-			const group = this._findGroupByPath(groupsPath, this._connections);
+			const group = this._findGroupByPath(groupsPath, this._root.getChildren());
 			if (!group) {
 				throw new Error(`Group path ${groupsPath.join(' -> ')} not found`);
 			}
@@ -90,14 +69,14 @@ export default class ConnectionManager {
 	public static deleteConnection(connection: Connection<any, any>): Promise<void>;
 	public static deleteConnection(connection: Connection<any, any>, groupsPath: string[]): Promise<void>;
 	public static async deleteConnection(connection: Connection<any, any>, groupsPath?: string[]): Promise<void> {
-		if (!this._connections) {
+		if (!this._root) {
 			await this.load();
 		}
-		assert(this._connections, 'Connections should be loaded before deleting one');
+		assert(this._root, 'Connections should be loaded before deleting one');
 		if (!groupsPath?.length) {
-			this._connections = this._connections.filter((c) => c.getName() !== connection.getName());
+			this._root.removeChild(connection);
 		} else {
-			const group = this._findGroupByPath(groupsPath, this._connections);
+			const group = this._findGroupByPath(groupsPath, this._root.getChildren());
 			if (!group) {
 				throw new Error(`Group path ${groupsPath.join(' -> ')} not found`);
 			}
@@ -107,7 +86,7 @@ export default class ConnectionManager {
 	}
 
 	public static getConnections(): (Connection<any, any> | ConnectionGroup)[] | null {
-		return this._connections;
+		return this._root ? this._root.getChildren() : null;
 	}
 
 	public static getConnectionsFilePath(): string {
@@ -119,16 +98,16 @@ export default class ConnectionManager {
 	}
 
 	public static clear(): void {
-		this._connections = null;
+		this._root = null;
 	}
 
 	private static async _save(): Promise<void> {
-		if (!this._connections) {
+		if (!this._root) {
 			return;
 		}
 		const configPath = this.getConnectionsFilePath();
 		await fs.mkdir(path.dirname(configPath), { recursive: true });
-		await fs.writeFile(configPath, JSON.stringify(this._connections, null, 4), 'utf-8');
+		await fs.writeFile(configPath, JSON.stringify(this._root.getChildren(), null, 4), 'utf-8');
 	}
 
 	private static _findGroupByPath(
